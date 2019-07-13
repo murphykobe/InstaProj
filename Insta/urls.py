05569f29e473e1bf077e37fdb29cf2908f72be90
Murphy Liu <murphykobe@gmail.com>
2019-07-13 18:21:56 -0400

    hello world page

--- Insta/urls.py
+++ Insta/urls.py
@@ -1,4 +1,4 @@
-"""InstaProj URL Configuration
+"""Insta URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from Insta.views import HelloDjango
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('Insta.urls'))
+    path('', HelloDjango.as_view() ,name='home'),
 ]
+ 
