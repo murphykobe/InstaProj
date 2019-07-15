29f7707a31a2ddfaf071479affafaa9ee5603fff
Murphy Liu <murphykobe@gmail.com>
2019-07-15 10:01:48 -0400

    update static root

--- InstaProj/settings.py
+++ InstaProj/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
